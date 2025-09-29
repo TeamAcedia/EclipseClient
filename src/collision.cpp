bfc111c99781a6d3f0f04e37d5b548cfe1d6d4cb
plus22 <minus22personalacc@gmail.com>
2025-09-28 22:52:27 -0300

    try to trigger github actions

--- src/collision.cpp
+++ src/collision.cpp
@@ -2,6 +2,7 @@
 // SPDX-License-Identifier: LGPL-2.1-or-later
 // Copyright (C) 2013 celeron55, Perttu Ahola <celeron55@gmail.com>
 
+
 #include "collision.h"
 #include <cmath>
 #include "irr_aabb3d.h"
